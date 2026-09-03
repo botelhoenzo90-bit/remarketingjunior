4e6d490090baeb7fa4ad43ebbd3340aae7232c41
botelhoenzo90-bit <botelhoenzo90@gmail.com>
2026-09-03 15:07:48 -0300

    Enxuga landing e mantém foco em objeções e conversão

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,18 +1,10 @@
 import { useEffect } from "react";
 import HeroSection from "@/components/landing/HeroSection";
 import RetargetingSection from "@/components/landing/RetargetingSection";
-import CoursePreviewSection from "@/components/landing/CoursePreviewSection";
 import FreeLessonsSection from "@/components/landing/FreeLessonsSection";
 import BenefitsSection from "@/components/landing/BenefitsSection";
-import PainPointsSection from "@/components/landing/PainPointsSection";
-import CourseContentSection from "@/components/landing/CourseContentSection";
-import BoardRepairSection from "@/components/landing/BoardRepairSection";
 import BonusSection from "@/components/landing/BonusSection";
 import TestimonialsSection from "@/components/landing/TestimonialsSection";
-import MentorSection from "@/components/landing/MentorSection";
-import CertificateSection from "@/components/landing/CertificateSection";
-import RecapSection from "@/components/landing/RecapSection";
-import RemarketingCloseSection from "@/components/landing/RemarketingCloseSection";
 import PricingSection from "@/components/landing/PricingSection";
 import FAQSection from "@/components/landing/FAQSection";
 import WhatsAppButton from "@/components/landing/WhatsAppButton";
@@ -26,18 +18,10 @@ const Index = () => {
     <div className="min-h-screen overflow-x-hidden bg-background">
       <HeroSection />
       <RetargetingSection />
-      <CoursePreviewSection />
       <FreeLessonsSection />
       <BenefitsSection />
-      <PainPointsSection />
-      <CourseContentSection />
-      <BoardRepairSection />
       <BonusSection />
       <TestimonialsSection />
-      <MentorSection />
-      <CertificateSection />
-      <RecapSection />
-      <RemarketingCloseSection />
       <PricingSection />
       <FAQSection />
       <WhatsAppButton />
